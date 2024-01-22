1db5c619e211f30e67286cc23b58b0f16d7bdb91
DevRajKandel <geniusppdevraj@gmail.com>
2024-01-22 06:03:01 +0545

    Update Second.php

--- Second.php
+++ Second.php
@@ -5,6 +5,8 @@ if($_REQUEST("q")!=="")
 if($_REQUEST("q")==="A")
 {
 echo ("Activated");
+  //Need to use sql connection to store complex data.
+  //From this point we can continue producing json
 }
 else
 {
