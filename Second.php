0f249e2403d7e7e30e5e16f0c7780da0b545c3f9
DevRajKandel <geniusppdevraj@gmail.com>
2024-01-22 15:01:46 +0545

    Update Second.php

--- Second.php
+++ Second.php
@@ -1,17 +1,17 @@
 <?php
-if($_REQUEST("q")!=="")
+if($_REQUEST["q"]!="") //Error!
 {
-
-if($_REQUEST("q")==="A")
+if($_REQUEST["q"]=="A")
 {
 echo ("Activated");
-  //Need to use sql connection to store complex data.
-  //From this point we can continue producing json
 }
 else
 {
 echo ("Not activated");
 }
-
+}
+else
+{
+    echo("Well no such input");
 }
 ?>
